c7781b63c26482a088b57110ec364f269117daa8
Juan Davila <juan.davila@unosquare.com>
2024-11-23 20:19:52 -0500

    Commit to evaluate how AWS code build works

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -4,4 +4,6 @@ test('test', async ({ page }) => {
   await page.goto('https://www.mercadolibre.com.co/');
   await page.getByPlaceholder('Buscar productos, marcas y má').fill('Iphone');
   await page.getByRole('button', { name: 'Buscar' }).click();
+  const locator = page.locator("//h1");
+  await expect(locator).toBeVisible()
 });
